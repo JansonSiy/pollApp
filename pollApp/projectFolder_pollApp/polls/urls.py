6eee026d53747ed78a8074c3e97ea54f2b6642f3
Janson Harold Siy <jansonharold.siy@gmail.com>
2021-11-26 14:37:20 +0800

    finished models, interactive shell, django admin and added views-urls-templates code

--- pollApp/projectFolder_pollApp/polls/urls.py
+++ pollApp/projectFolder_pollApp/polls/urls.py
@@ -2,6 +2,22 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
+    # example: /polls/
     path('', views.index, name='index'),
+    # example: /polls/1/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # example: /polls/1/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # example: /polls/1/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
+
+# NOTES THAT SHOULD BE IN polls/templates/polls/index.html
+# notes: hardcoded
+# <li><a href="/polls/{{ question.id }}/">{{ question.question_text }}</a></li>
+# notes: using polls.urls.py
+# <li><a href="{% url 'detail' question.id %}">{{ question.question_text }}</a></li>
+# notes: using app_name in urls.py
+# <li><a href="{% url 'polls:detail' question.id %}">{{ question.question_text }}</a></li>
