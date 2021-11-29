f803bc85336d636d1270241bc00e261379259362
Janson Harold Siy <jansonharold.siy@gmail.com>
2021-11-29 13:26:08 +0800

    finished testing

--- pollApp/projectFolder_pollApp/polls/urls.py
+++ pollApp/projectFolder_pollApp/polls/urls.py
@@ -4,15 +4,23 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    # example: /polls/
-    path('', views.index, name='index'),
-    # example: /polls/1/
-    path('<int:question_id>/', views.detail, name='detail'),
-    # example: /polls/1/results/
-    path('<int:question_id>/results/', views.results, name='results'),
-    # example: /polls/1/vote/
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
+
+# app_name = 'polls'
+# urlpatterns = [
+#     # example: /polls/
+#     path('', views.index, name='index'),
+#     # example: /polls/1/
+#     path('<int:question_id>/', views.detail, name='detail'),
+#     # example: /polls/1/results/
+#     path('<int:question_id>/results/', views.results, name='results'),
+#     # example: /polls/1/vote/
+#     path('<int:question_id>/vote/', views.vote, name='vote'),
+# ]
 
 # NOTES THAT SHOULD BE IN polls/templates/polls/index.html
 # notes: hardcoded
--- pollApp/projectFolder_pollApp/polls/views.py
+++ pollApp/projectFolder_pollApp/polls/views.py
@@ -1,6 +1,7 @@
 # notes: from django.shortcuts import render
 # from django.http import HttpResponse
-from django.http import HttpResponse, HttpResponseRedirect
+# from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 
 # notes: longcut index
 # from django.template import loader
@@ -12,10 +13,14 @@ from django.shortcuts import render
 # notes: shorthand details
 from django.shortcuts import get_object_or_404, render
 
+from django.urls import reverse
+
+from django.views import generic
+
+from django.utils import timezone
+
 # from .models import Question
 from .models import Choice, Question
-
-from django.urls import reverse
 
 # Create your views here.
 
@@ -38,10 +43,24 @@ from django.urls import reverse
 #     return HttpResponse(template.render(context, request))
 
 # notes: shorthand index
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     context = {'latest_question_list': latest_question_list}
+#     return render(request, 'polls/index.html', context)
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+    def get_queryset(self):
+        """Return the last five published questions (not including those set to be published in the future)."""
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+
+
 
 # notes: dummy code
 # def detail(request, question_id):
@@ -56,18 +75,38 @@ def index(request):
 #     return render(request, 'polls/detail.html', {'question': question})
 
 # notes: shorthand detail
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/detail.html', {'question': question})
+
+# class DetailView(generic.DetailView):
+#     model = Question
+#     template_name = 'polls/detail.html'
+
+class DetailView(generic.DetailView):
+    ...
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+
 
 # notes: dummy code
 # def results(request, question_id):
 #     response = "You're looking at the results of question %s."
 #     return HttpResponse(response % question_id)
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {'question': question})
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
 
 # notes: dummy code
 # def vote(request, question_id):
